d19cf4fa0d652b3228bdc6279e352dbb5e9b7aaa
Anna Kummer <annamkummer@gmail.com>
2021-11-01 20:55:18 -0600

    Add tests and methods for user activity averages

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -24,6 +24,44 @@ class UserRepository {
 
     return Number(average.toFixed(2))
   }
+
+// AKU START ========================
+  calculateAvgStairsClimbed(activityData, date) {
+    const sum = activityData.reduce((stairs, entry) => {
+      if (entry.date === date) {
+        stairs.stairs += entry.flightsOfStairs;
+        stairs.count += 1;
+      }
+      return stairs;
+    }, {stairs: 0, count: 0});
+    const avg = sum.stairs / sum.count;
+    return Number(avg.toFixed(2))
+  }
+
+  calculateAvgStepsTaken(activityData, date) {
+    const sum = activityData.reduce((steps, entry) => {
+      if (entry.date === date) {
+        steps.steps += entry.numSteps;
+        steps.count += 1;
+      }
+      return steps;
+    }, {steps: 0, count: 0});
+    const avg = sum.steps / sum.count;
+    return Number(avg.toFixed(2))
+  }
+
+  calculateAvgMinActive(activityData, date) {
+    const sum = activityData.reduce((minutes, entry) => {
+      if (entry.date === date) {
+        minutes.minutes += entry.minutesActive;
+        minutes.count += 1;
+      }
+      return minutes;
+    }, {minutes: 0, count: 0});
+    const avg = sum.minutes / sum.count;
+    return Number(avg.toFixed(2))
+  }
+// AKU END =========================
 }
 
 export default UserRepository;
--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import UserRepository from '../src/UserRepository';
 
 describe('User Repository', () => {
-  let userData, userRepository;
+  let userData, userRepository, activityData;
 
   beforeEach(function() {
     userData = [
@@ -34,6 +34,73 @@ describe('User Repository', () => {
         "friends": [19, 11, 42, 33]
       }]
     userRepository = new UserRepository(userData);
+
+// AKU START ========================
+    activityData = [{
+      "userID": 1,
+      "date": "2019/06/15",
+      "numSteps": 3577,
+      "minutesActive": 140,
+      "flightsOfStairs": 16
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/16",
+      "numSteps": 6637,
+      "minutesActive": 175,
+      "flightsOfStairs": 37
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/17",
+      "numSteps": 14329,
+      "minutesActive": 168,
+      "flightsOfStairs": 18
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/15",
+      "numSteps": 4419,
+      "minutesActive": 165,
+      "flightsOfStairs": 33
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/16",
+      "numSteps": 8429,
+      "minutesActive": 243,
+      "flightsOfStairs": 44
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/17",
+      "numSteps": 14478,
+      "minutesActive": 140,
+      "flightsOfStairs": 12
+    },
+    {
+      "userID": 3,
+      "date": "2019/06/15",
+      "numSteps": 6760,
+      "minutesActive": 135,
+      "flightsOfStairs": 6
+    },
+    {
+      "userID": 3,
+      "date": "2019/06/16",
+      "numSteps": 10289,
+      "minutesActive": 119,
+      "flightsOfStairs": 6
+    },
+    {
+      "userID": 3,
+      "date": "2019/06/17",
+      "numSteps": 4294,
+      "minutesActive": 138,
+      "flightsOfStairs": 10
+    },
+    ];
+// AKU END =========================
   });
 
   it('should be a function', function () {
@@ -48,4 +115,18 @@ describe('User Repository', () => {
   it('should display average step goal amongst all users', function () {
     expect(userRepository.calculateAvgStepGoal()).to.equal(6666.67);
   });
+
+  // AKU START ========================
+  it('should return average stairs climbed on specific date among all users', function () {
+    expect(userRepository.calculateAvgStairsClimbed(activityData, '2019/06/15')).to.equal(18.33);
+  })
+
+  it('should return average steps taken on specific date among all users', function () {
+    expect(userRepository.calculateAvgStepsTaken(activityData, '2019/06/16')).to.equal(8451.67);
+  })
+
+  it('should return average minutes active on specific date among all users', function () {
+    expect(userRepository.calculateAvgMinActive(activityData, '2019/06/17')).to.equal(148.67);
+  })
+  // AKU END =========================
 });
