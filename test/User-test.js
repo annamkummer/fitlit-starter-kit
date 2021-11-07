bc4b445a149b35e352aad6acc24a3fa244ac65c5
Adam Burgess <aburg1515@gmail.com>
2021-11-07 09:35:47 -0700

    Update test file for missing tests

--- test/User-test.js
+++ test/User-test.js
@@ -295,4 +295,16 @@ describe('User', () => {
     expect(user2.findStairRecord(activityData)).to.equal(37);
   })
 
+  it('should calculate weekly steps average', function() {
+    expect(user1.calculateWeeklySteps(activityData, "2019/06/21")).to.equal(8375.57);
+  })
+
+  it('should calculate weekly flights average', function() {
+    expect(user1.calculateWeeklyFlights(activityData, "2019/06/21")).to.equal(23.71);
+  })
+
+  it('should calculate weekly miles', function() {
+    expect(user1.calculateWeeklyMiles(activityData, "2019/06/21")).to.equal(6.82);
+  })
+
 });
